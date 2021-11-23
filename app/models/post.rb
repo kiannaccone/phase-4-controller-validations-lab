8a583d8f6e15c3a78d64450d7556f9d735ca6253
Katherine Iannaccone <katherineiannaccone@yahoo.com>
2021-11-23 11:07:29 -0700

    commit tree 49dd534156a1eeebc8101efdf2753bbf31cf49ba

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 100}
+    validates :category, presence: true, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    # validates :content, length: {minimum: 100}
 end
